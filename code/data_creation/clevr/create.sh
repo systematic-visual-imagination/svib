c5165a2d12332ce85a6d9a01e254a76043083733
systematic-visual-imagination <systematic-visual-imagination@proton.me>
2023-09-22 14:54:47 +0900

    Refactoring: CLEVR&CLEVRTex

--- code/data_creation/clevr/create.sh
+++ code/data_creation/clevr/create.sh
@@ -1,5 +1,5 @@
 i=0
-while [ "$i" -le 100 ]; do
-    /common/home/gs790/blender-2-78/blender --background --python create_data.py -- --rule $1 --save_path $2 --bind_path $3
+while [ "$i" -lt 100 ]; do
+    ./blender-2.78-linux-glibc219-x86_64/blender --background --python create_data.py -- --rule $1 --save_path $2 --bind_path $3
     i=$(( i + 1 ))
 done
